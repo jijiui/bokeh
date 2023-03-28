a9ffea95009f7377e344aa6aa3f239a4788b0c22
Mateusz Paprocki <mattpap@gmail.com>
2023-03-28 15:51:33 +0200

    Update CustomJS' unit tests

--- bokehjs/test/unit/models/callbacks/customjs.ts
+++ bokehjs/test/unit/models/callbacks/customjs.ts
@@ -1,24 +1,16 @@
+import * as sinon from "sinon"
 import {expect} from "assertions"
 
 import {CustomJS} from "@bokehjs/models/callbacks/customjs"
 import {Range1d} from "@bokehjs/models/ranges/range1d"
-import {Document} from "@bokehjs/document"
-import {version as js_version} from "@bokehjs/version"
+import {GeneratorFunction, AsyncGeneratorFunction} from "@bokehjs/core/types"
+import {logger} from "@bokehjs/core/logging"
+//import {Document} from "@bokehjs/document"
+//import {version as js_version} from "@bokehjs/version"
 
 describe("CustomJS", () => {
 
-  describe("default constructor", () => {
-    const r = new CustomJS()
-
-    it("should have empty args", () => {
-      expect(r.args).to.be.equal({})
-    })
-
-    it("should have empty code", () => {
-      expect(r.code).to.be.equal("")
-    })
-  })
-
+  /*
   describe("values property", () => {
     const rng = new Range1d()
     const r = new CustomJS({args: {foo: rng}})
@@ -46,64 +38,129 @@ describe("CustomJS", () => {
       expect(r.values).to.be.equal([rng2])
     })
   })
+  */
 
-  describe("func property", () => {
+  describe("state property", () => {
 
-    it("should return a Function", () => {
-      const r = new CustomJS()
-      expect(r.func).to.be.instanceof(Function)
+    it("should support JS code", async () => {
+      const cb = new CustomJS({code: "return 10"})
+      const {func, module} = await cb.state()
+      expect(func).to.be.instanceof(Function)
+      expect(func.toString()).to.be.equal("function () { [native code] }") // due to bind()
+      expect(module).to.be.equal(false)
     })
 
-    it("should have code property as function body", () => {
-      const r = new CustomJS({code: "return 10"})
-      const f = new Function("cb_obj", "cb_data", "'use strict';\nreturn 10")
-      expect(r.func.toString()).to.be.equal(f.toString())
+    it("should support ES module with a default function", async () => {
+      const cb = new CustomJS({code: "export default function(args, obj, data) { return 10 }"})
+      const {func, module} = await cb.state()
+      expect(func).to.be.instanceof(Function)
+      expect(func.toString()).to.be.equal("function(args, obj, data) { return 10 }")
+      expect(module).to.be.equal(true)
     })
 
-    it("should have values as function args", () => {
-      const rng = new Range1d()
-      const r = new CustomJS({args: {foo: rng.ref()}, code: "return 10"})
-      const f = new Function("foo", "cb_obj", "cb_data", "'use strict';\nreturn 10")
-      expect(r.func.toString()).to.be.equal(f.toString())
+    it("should support ES module with a default arrow function", async () => {
+      const cb = new CustomJS({code: "export default (args, obj, data) => 10"})
+      const {func, module} = await cb.state()
+      expect(func).to.be.instanceof(Function)
+      expect(func.toString()).to.be.equal("(args, obj, data) => 10")
+      expect(module).to.be.equal(true)
+    })
+
+    it("should support ES module with a default async function", async () => {
+      const cb = new CustomJS({code: "export default async function(args, obj, data) { return 10 }"})
+      const {func, module} = await cb.state()
+      expect(func).to.be.instanceof(Function)
+      expect(func.toString()).to.be.equal("async function(args, obj, data) { return 10 }")
+      expect(module).to.be.equal(true)
+    })
+
+    it("should support ES module with a default async arrow function", async () => {
+      const cb = new CustomJS({code: "export default async (args, obj, data) => 10"})
+      const {func, module} = await cb.state()
+      expect(func).to.be.instanceof(Function)
+      expect(func.toString()).to.be.equal("async (args, obj, data) => 10")
+      expect(module).to.be.equal(true)
+    })
+
+    it("should support ES module with a default generator function", async () => {
+      const cb = new CustomJS({code: "export default function*(args, obj, data) { yield 5; return 10 }"})
+      const {func, module} = await cb.state()
+      expect(func).to.be.instanceof(GeneratorFunction)
+      expect(func.toString()).to.be.equal("function*(args, obj, data) { yield 5; return 10 }")
+      expect(module).to.be.equal(true)
+    })
+
+    it("should support ES module with a default async generator function", async () => {
+      const cb = new CustomJS({code: "export default async function*(args, obj, data) { yield 5; return 10 }"})
+      const {func, module} = await cb.state()
+      expect(func).to.be.instanceof(AsyncGeneratorFunction)
+      expect(func.toString()).to.be.equal("async function*(args, obj, data) { yield 5; return 10 }")
+      expect(module).to.be.equal(true)
+    })
+
+    it("should support ES module bad deafult export", async () => {
+      const cb = new CustomJS({code: "const some = 10;\nexport default some"})
+      const logger_spy = sinon.spy(logger, "warn")
+      try {
+        const {func, module} = await cb.state()
+        expect(func).to.be.instanceof(Function)
+        expect(func.toString()).to.be.equal("() => undefined")
+        expect(module).to.be.equal(true)
+        expect(logger_spy.calledOnceWith("custom ES module didn't export a default function"))
+      } finally {
+        logger_spy.restore()
+      }
     })
   })
 
   describe("execute method", () => {
 
-    it("should execute the code and return the result", () => {
+    it("should execute the code and return the result", async () => {
       const r = new CustomJS({code: "return 10"})
-      expect(r.execute("foo")).to.be.equal(10)
+      const obj = new Range1d({start: 1, end: 2})
+      expect(await r.execute(obj)).to.be.equal(10)
     })
 
-    it("should execute the code with args parameters passed", () => {
+    it("should execute the code with args parameters passed", async () => {
       const r = new CustomJS({args: {foo: 5}, code: "return 10 + foo"})
-      expect(r.execute("foo")).to.be.equal(15)
+      const obj = new Range1d({start: 1, end: 2})
+      expect(await r.execute(obj)).to.be.equal(15)
     })
 
-    it("should return the cb_obj passed an args parameter to execute", () => {
+    it("should return the cb_obj passed an args parameter to execute", async () => {
       const r = new CustomJS({code: "return cb_obj"})
-      expect(r.execute("foo")).to.be.equal("foo")
+      const obj = new Range1d({start: 1, end: 2})
+      expect(await r.execute(obj)).to.be.equal(obj)
     })
 
-    it("should return cb_data with default value if cb_data kwarg is unset", () => {
+    it("should return cb_data with default value if cb_data kwarg is unset", async () => {
       const r = new CustomJS({code: "return cb_data"})
-      expect(r.execute("foo")).to.be.equal({})
+      const obj = new Range1d({start: 1, end: 2})
+      expect(await r.execute(obj)).to.be.equal({})
     })
 
-    it("should return cb_data with value of kwarg parameter to execute", () => {
+    it("should return cb_data with value of kwarg parameter to execute", async () => {
       const r = new CustomJS({code: "return cb_data.foo"})
-      expect(r.execute("foo", {foo: "bar"})).to.be.equal("bar")
+      const obj = new Range1d({start: 1, end: 2})
+      expect(await r.execute(obj, {foo: "bar"})).to.be.equal("bar")
     })
 
-    it("should execute the code with args parameters correctly mapped", () => {
-      // the point of this test is that we shouldn't be relying on
-      // the definition order of keys in a JS object, though it
-      // is reliable in some JS runtimes
-      const r = new CustomJS({args: {
-        foo4: "foo4", foo5: "foo5", foo6: "foo6",
-        foo1: "foo1", foo2: "foo2", foo3: "foo3",
-      }, code: "return foo1 + foo2 + foo3 + foo4 + foo5 + foo6"})
-      expect(r.execute({})).to.be.equal("foo1foo2foo3foo4foo5foo6")
+    it("should execute the code with args parameters correctly mapped", async () => {
+      // The point of this test is that we shouldn't be relying on the definition
+      // order of keys in a JS object, though it is reliable in some JS runtimes.
+      const r = new CustomJS({
+        args: {
+          foo4: "foo4",
+          foo5: "foo5",
+          foo6: "foo6",
+          foo1: "foo1",
+          foo2: "foo2",
+          foo3: "foo3",
+        },
+        code: "return [foo1, foo2, foo3, foo4, foo5, foo6]",
+      })
+      const obj = new Range1d({start: 1, end: 2})
+      expect(await r.execute(obj)).to.be.equal(["foo1", "foo2", "foo3", "foo4", "foo5", "foo6"])
     })
   })
 })
